d96ebea65a34eee4168d22e422598b8a3f98647b
Ahmet Onur Yılmaz <ahmetonur.yilmaz@C02ZF5AALVDQ.local>
2020-10-04 16:58:29 +0300

    prevents backpressure

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/Shopify/sarama v1.26.1
 	github.com/Trendyol/kafka-wrapper v0.0.0-20200329203808-48e5b8b2c69d
+	github.com/docker/distribution v2.7.1-0.20190205005809-0d3efadf0154+incompatible
 	github.com/gin-gonic/gin v1.6.2
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/robfig/cron v1.2.0
--- services/handler.go
+++ services/handler.go
@@ -5,17 +5,22 @@ import (
 	"fmt"
 	"github.com/Shopify/sarama"
 	"github.com/Trendyol/kafka-shovel/kafka"
+	"github.com/docker/distribution/uuid"
 )
+
+const runningKeyHeader = "running_key"
 
 type Interceptor func(ctx context.Context, message *sarama.ConsumerMessage) context.Context
 
 type eventHandler struct {
-	Service
+	runningKey string
+	service    Service
 }
 
 func NewEventHandler(service Service) kafka.EventHandler {
 	return &eventHandler{
-		service,
+		service:    service,
+		runningKey: uuid.Generate().String(),
 	}
 }
 
@@ -30,8 +35,12 @@ func (e *eventHandler) Cleanup(sarama.ConsumerGroupSession) error {
 func (e *eventHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
 		fmt.Printf("Received key: %s, topic: %s \n", string(message.Key), message.Topic)
+		if e.doesMessageProcessed(message) {
+			fmt.Printf("Message already is processed. Shovel execution halted.  key: %s, topic: %s \n", string(message.Key), message.Topic)
+			return nil
+		}
 
-		err := e.Service.OperateEvent(context.Background(), message)
+		err := e.service.OperateEvent(context.Background(), message)
 		if err != nil {
 			fmt.Println("Error executing err: ", err)
 		}
@@ -40,4 +49,23 @@ func (e *eventHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim s
 	}
 
 	return nil
+}
+
+func (e *eventHandler) doesMessageProcessed(message *sarama.ConsumerMessage) bool {
+	for _, header := range message.Headers {
+		if string(header.Key) == runningKeyHeader {
+			if string(header.Value) == e.runningKey {
+				return true
+			} else {
+				header.Value = []byte(e.runningKey)
+				return false
+			}
+		}
+	}
+
+	message.Headers = append(message.Headers, &sarama.RecordHeader{
+		Key:   []byte(runningKeyHeader),
+		Value: []byte(e.runningKey),
+	})
+	return false
 }
